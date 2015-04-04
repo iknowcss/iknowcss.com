7915b7fd8da67850158d98bfab7070247a5db846
Cody Jenkins <iknowcss@gmail.com>
2015-04-03 20:29:41 -0700

    First draft

--- public/js/rule-30.js
+++ public/js/rule-30.js
@@ -1,39 +1,116 @@
 (function (ik) {
 
+  var WORD = 31;
+
+  /// - WolframRow -------------------------------------------------------------
+
+  ik.WolframRow = WolframRow;
+
+  function WolframRow(cols) {
+    this.length = _.isNumber(cols) ? cols : cols.length;
+
+    // How many Number blocks do we need at WORD bits each?
+    this.blockCount = Math.ceil(this.length / WORD);
+
+    // Create array of bit blocks and zero them out
+    this.blocks = new Array(this.blockCount);
+    _.each(this.blocks, function (b, i) {
+      this.blocks[i] = 0;
+    }, this);
+
+    // Init
+    if (!_.isNumber(cols)) {
+      this.setRange(cols);
+    }
+  }
+
+  _.extend(WolframRow.prototype, {
+
+    print: function () {
+      var output = '';
+      for (var i = 0; i < this.length; i++) {
+        output += this.get(i) ? '1' : '0';
+      }
+      return output;
+    },
+
+    get: function (_i) {
+      var i = (_i < 0 ? this.length + _i % this.length : _i % this.length),
+          block = Math.floor(i / WORD),
+          blockValue = this.blocks[block],
+          bit = i % WORD;
+      return (blockValue & (1 << bit)) > 0;
+    },
+
+    set: function (i, d) {
+      var block = Math.floor(i / WORD),
+          blockValue = this.blocks[block],
+          bit = i % WORD;
+      this.blocks[block] ^= (-(d ? 1 : 0) ^ blockValue) & (1 << bit);
+      return this;
+    },
+
+    toggle: function (i) {
+      this.set(i, !this.get(i));
+    },
+
+    setRange: function (_input, _offset) {
+      var input = _input,
+          offset = _offset ? _offset : 0;
+      if (_.isString(input)) {
+        input = _.map(input.split(''), function (d) {
+          return d === '1';
+        });
+      }
+      _.each(input, function (d, i) {
+        this.set(i + offset, !!d);
+      }, this);
+      return this;
+    },
+
+    each: function (fn, ctx) {
+      for (var i = 0; i < this.length; i++) {
+        fn.call(ctx, this.get(i), i);
+      }
+    },
+
+    slide: function (fn, ctx) {
+      for (var i = 0; i < this.length; i++) {
+        fn.call(ctx, this.get(i - 1), this.get(i), this.get(i + 1), i);
+      }
+    },
+
+    clone: function () {
+      var clone = new WolframRow(this.length)
+      clone.blocks = _.clone(this.blocks);
+      return clone;
+    }
+
+  });
+
   /// - WolframRunner ----------------------------------------------------------
 
   ik.WolframRunner = WolframRunner;
 
-  WolframRunner.Midpoint = function (blocks) {
-    var midpoint = Math.floor(blocks / 2);
-    return new WolframRunner(_.map(new Array(blocks), function (d, i) {
-      return i === midpoint ? 1 : 0;
-    }));
-  };
-
   function WolframRunner(seed) {
-    if (_.isArray(seed)){
-      this.seed = seed;
-    } else if (_.isString(seed)) {
-      this.seed = seed.split('').map(function (d) {
-        return d === '1';
-      });
+    if (!(this instanceof WolframRunner)) {
+      return new WolframRunner(seed);
     }
-    this.current = this.seed;
+
+    this.initial = this.current = new WolframRow(seed);
+    this.reset();
   }
 
   _.extend(WolframRunner.prototype, {
 
     step: function () {
-      var len = this.current.length,
-          data = new Array(len);
+      var len = this.current.length;
       this.previous = this.current;
-      this.current = [];
-      this.previous.forEach(function (d, i) {
-        var p = this.previous[i === 0 ? len - 1 : i - 1],
-            q = this.previous[i],
-            r = this.previous[i === len - 1 ? 0 : i + 1];
-        this.current[i] = (p + q + r + q * r) % 2 === 1;
+      this.current = new WolframRow(len);
+      this.previous.slide(function (p, q, r, i) {
+        var value = (p + q + r + q * r) % 2 === 1;
+        // var value = (q + r + q * r + p * q * r) % 2 === 1;
+        this.current.set(i, value);
       }, this);
       return this.current;
     },
@@ -42,6 +119,11 @@
       var cur = this.current;
       this.step();
       return cur;
+    },
+
+    reset: function () {
+      this.current = this.initial;
+      delete this.previous;
     }
 
   });
@@ -57,10 +139,12 @@
     this.height = canvas.height;
     this.cols = Math.ceil(this.width / blockSize);
     this.rows = Math.ceil(this.height / blockSize);
+    this.painted = [];
 
     this.pxScale = function (blocks) {
       return blocks * blockSize;
     };
+    this.unit = this.pxScale(1);
 
     this.reset();
   }
@@ -68,35 +152,43 @@
   _.extend(WolframPainter.prototype, {
 
     paintSquare: function (x, y, c) {
-      var unit = this.pxScale(1);
-      this.context.fillStyle = 'rgb(' + c[0] + ',' + c[1] + ',' + c[2] + ')';
-      this.context.fillRect(this.pxScale(x), this.pxScale(y), unit, unit);
+      var self = this,
+          paintX = this.pxScale(x),
+          paintY = this.pxScale(y);
+      // setTimeout(function () {
+        self.context.fillStyle = 'rgb(' + c[0] + ',' + c[1] + ',' + c[2] + ')';
+        self.context.fillRect(paintX, paintY, self.unit, self.unit);
+      // }, Math.random() * 100);
     },
 
-    paintRow: function (row) {
-      _.each(row, function (d, i) {
-        var color = d ? [0, 0, 0] : [255, 255, 255];
+    paintRow: function (row, _color) {
+      row.each(function (d, i) {
+        // Skip painting pixels that were already painted as "on"
+        var paintedRow = this.painted[this.scanLine];
+        if (d && paintedRow && paintedRow.get(i)) {
+          console.log('no at ' + i + ', ' + this.scanLine)
+          return;
+        }
+
+        var color = d ? (_color || [0, 0, 0]) : [255, 255, 255];
         this.paintSquare(i, this.scanLine, color);
       }, this);
+      this.painted[this.scanLine] = row.clone();
       this.scanLine++;
     },
 
-    paintWithRunner: function (runner, rows, interval) {
-      if (interval > 0) {
-        return this.paintWithRunnerInterval(runner, rows, interval);
-      }
-
+    paintWithRunner: function (runner, rows, color) {
       for (var i = 0; i < rows; i++) {
-        this.paintRow(runner.getCurrentAndStep());
+        this.paintRow(runner.getCurrentAndStep(), color);
       }
     },
 
-    paintWithRunnerInterval: function (runner, rows, interval) {
+    paintWithRunnerInterval: function (runner, rows, color, interval) {
       var self = this,
           i = 0;
       var h = setInterval(function () {
         if (i++ < rows) {
-          self.paintRow(runner.getCurrentAndStep());
+          self.paintRow(runner.getCurrentAndStep(), color);
         } else {
           clearInterval(h);
         }
